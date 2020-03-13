97d46239a335856356a4e9ee913255729c1e04a2
Patrick Bittman <pat.bittman@gmail.com>
2020-03-13 13:18:47 -0700

    first working implementation of cmd_in structure with underlying python functions

---
+++ home.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+import ret
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    no_cmd = "Command output will appear here."
+    return render_template('index_home.html', cmd_out=no_cmd)
+    
+@app.route('/new_cmd', methods = ['POST'])
+def new_cmd_in():
+    cmd_string = request.form['cmd_box']
+    action, data = process_cmd(cmd_string)
+    if action == ret.HOME:
+        return render_template('index_home.html', cmd_out=data)
+    
+def process_cmd(cmd_string):
+    if not check_syntax(cmd_string):
+        return ret.HOME, "Invalid command entry: " + cmd_string
+    else:
+        return ret.HOME, "Valid command entry: " + cmd_string
+
+def check_syntax(cmd_string):
+    return cmd_string.find('=') >= 0
+
+if __name__ == '__main__':
+   app.run()
+
+"""
+action, data = process_cmd("disp_char=Rand")
+print(action)
+print(data)
+"""
---
+++ ret.py
@@ -0,0 +1,8 @@
+#Generic Errors
+ERROR = -1
+SUCCESS = 0
+INVALID_INPUT = -2
+
+#Website Directions
+HOME = 0
+CHAR_FORM = 1
