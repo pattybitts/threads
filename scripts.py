ed5cdf6e741d97eb54e0c5196667e1c135345094
Patrick Bittman <pat.bittman@gmail.com>
2020-05-17 02:24:16 -0500

    added chapter parsing script. not officially added to object

--- scripts.py
+++ scripts.py
@@ -1,10 +1,13 @@
-import ret, data_storage, log
+import ret, data_storage, log, copy, re
 from cmd import ValidCommands
 from Series import Series
+from Book import Book
+from Chapter import Chapter
+from Viewpoint import Viewpoint
 from Character import Character
 
 #eliminate allegiance property and instead add "Darkfriend" tag to shadow allies
-if 1:
+if 0:
     series_info = data_storage.load_pickle(data_storage.ACTIVE_FILE)
     new_series_info = Series()
     for c in series_info.characters:
@@ -13,4 +16,70 @@ if 1:
             new_char.tags.append("Darkfriend")
         new_series_info.add_char(new_char)
     new_series_info.save("wot_0")
+
+#import book, viewpoints, words, characters to series class
+if 1:
+    series = data_storage.load_pickle(data_storage.ACTIVE_FILE)
+    input = open("eotw.txt", 'r')
+    full_text = input.read()
+    lines = full_text.split("\n")
+    book = Book("Eye of the World", 1)
+    ch_count = 0
+    vp_count = 0
+    chapter = None
+    for line in lines:
+        cells = line.split(",")
+        num_cells = len(cells)
+        character = series.find_char(cells[1])
+        ch_name = cells[0]
+        if ch_name != '':
+            vp_count = 1
+        else:
+           vp_count += 1
+        if character == ret.ERROR:
+            log.out("Unable to locate character: " + cells[1])
+            continue
+        viewpoint = Viewpoint(vp_count, character, cells[2])
+        for i in range(3, num_cells):
+            char_name = cells[i]
+            if char_name == '':
+                break
+            character = series.find_char(char_name)
+            if character == ret.ERROR:
+                log.out("Unable to locate character: " + cells[i])
+                continue
+            viewpoint.add_featured(copy.copy(character))
+        #at this point the viewpoint object is complete
+        if ch_name != '':
+            if not chapter is None:
+                book.add_chapter(copy.copy(chapter))
+            vp_re = re.match("(\S+): (.*)", str(cells[0]))
+            if not vp_re:
+                log.out("Unable to match chapter name: " + str(cells[0]))
+                continue
+            if vp_re.group(1) == "Prologue":
+                ch_count = 0
+                ch_title = vp_re.group(0)
+            else:
+                ch_count += 1
+                ch_title = vp_re.group(2)
+            chapter = Chapter(ch_title, ch_count)
+        if not chapter is None:
+            chapter.add_viewpoint(copy.copy(viewpoint))
+    book.add_chapter(copy.copy(chapter))
+    for ch in book.chapters:
+        log.out(ch.print_chapter())
+
+if 0:
+    booklist = []
+    book = Book("Original", 1)
+    booklist.append(copy.deepcopy(book))
+    book = Book("Edit", 2)
+    booklist.append(copy.deepcopy(book))
+    log.db("book", booklist[0].title)
+    log.db("copied_book", booklist[1].title)
+
+
+
+
 
